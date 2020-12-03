d2b70ebf04d8c028486f30014c5964b4300cdd16
narmadakandhikattu <narmadait.2006@gmail.com>
2020-12-03 03:18:36 -0800

    Initial Version 1

--- myApp.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-var orders = fs.readFileSync("order.json");
-var jsonContent = JSON.parse(orders);
-console.log("Order Id :", jsonContent.orderId);
-console.log("orderStatus:", jsonContent.orderStatus);
- 
